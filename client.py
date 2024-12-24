2ba48d3ebc70a824365a2d4b64fc38d814bd6d4f
Sitam <sitammeur@gmail.com>
2024-12-24 21:43:49 +0530

    server and client code added

---
+++ client.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def test_server(input_text):
+    """
+    Sends a POST request to the server with the given input text and prints the server's response.
+
+    Args:
+        input_text (str): The text to be sent to the server for prediction.
+
+    Returns: None
+    """
+    # API endpoint URL for the server
+    url = "http://127.0.0.1:8000/predict"
+
+    # Send a POST request with the request payload
+    payload = {"input_text": input_text}
+    response = requests.post(url, json=payload)
+
+    # Print the response from the server
+    print(response.json())
+
+
+if __name__ == "__main__":
+    # Sample input text for testing. e.g., "The capital of France is [MASK]."
+    input_text = "He walked to the [MASK]."
+    test_server(input_text)
---
+++ server.py
@@ -0,0 +1,57 @@
+# Import necessary libraries
+import torch
+from transformers import pipeline
+import litserve as ls
+
+
+class ModernBertAPI(ls.LitAPI):
+    """
+    ModernBertAPI is a subclass of ls.LitAPI that provides an interface to a BERT-based model for the "fill-mask" task.
+
+    Methods:
+        - setup(device): Initializes the pipeline with the specified device.
+        - decode_request(request): Decodes the input request to extract the input text.
+        - predict(input_text): Uses the pipeline to predict the masked tokens in the input text.
+        - encode_response(results): Encodes the prediction results into a dictionary format.
+    """
+
+    def setup(self, device):
+        """
+        Sets up the pipeline for the fill-mask task using the specified device.
+        """
+        # Initialize the pipeline with the specified device
+        model_name = "answerdotai/ModernBERT-large"
+        self.pipeline = pipeline(
+            "fill-mask",
+            model=model_name,
+            torch_dtype=torch.bfloat16,
+            device=device,
+        )
+
+    def decode_request(self, request):
+        """
+        Decodes the input request to extract the input text.
+        """
+        # Extract the input text from the request
+        return request["input_text"]
+
+    def predict(self, input_text):
+        """
+        Generates a prediction based on the provided input text.
+        """
+        # Generate a prediction using the pipeline
+        return self.pipeline(input_text)
+
+    def encode_response(self, results):
+        """
+        Encodes the given results into a dictionary format.
+        """
+        # Return the results in a dictionary format
+        return {"token": results[0]["token_str"], "sequence": results[0]["sequence"]}
+
+
+if __name__ == "__main__":
+    # Create an instance of the ModernBertAPI and run the LitServer
+    api = ModernBertAPI()
+    server = ls.LitServer(api, track_requests=True)
+    server.run(port=8000)
